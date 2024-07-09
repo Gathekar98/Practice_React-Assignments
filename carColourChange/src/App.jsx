fe3780b226eaca273c7365944caa60ab6bbc5117
Gathekar98 <gathekar.ashwini@gmail.com>
2024-07-09 15:48:40 +0530

    car color changes commit

---
+++ carColourChange/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [car, setCar] = useState({
+    brand: "Ferrari",
+    color: "Red",
+    modal: "Roma",
+    year: "2023",
+  });
+
+  const changeColor = () => {
+    setCar((car) => {
+      return { ...car, color: "Blue"};
+    });
+  };
+
+  return (
+    <>
+      <h1>My {car.brand}</h1>
+      <h2>
+        It is {car.color} {car.modal} from {arguments.year}
+      </h2>
+      <button onClick={changeColor}>Blue</button>
+    </>
+  );
+}
+
+export default App;
